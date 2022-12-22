636bba27d38bba42572f56c43a5b88cae60bd841
Binh2 <hgqbinh2002@gmail.com>
2022-12-22 17:26:28 +0700

    Add a some properties to Food Add a new Restaurant class

--- FoodOrderingApp/FoodOrderingApp/Model/Food.cs
+++ FoodOrderingApp/FoodOrderingApp/Model/Food.cs
@@ -4,12 +4,14 @@ using System.Text;
 using SQLite;
 namespace FoodOrderingApp.Model
 {
-     public class Food
+    public class Food
     {
         [PrimaryKey,AutoIncrement]
         public int FoodID { get; set; }
         public string FoodName { get; set; }
         public string FoodImages { get; set; }
+        public string FoodQuantity { get; set; }
         public int CategoryID { get; set; }
+        public int RestaurantID { get; set; }
     }
 }
---
+++ FoodOrderingApp/FoodOrderingApp/Model/Restaurant.cs
@@ -0,0 +1,14 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FoodOrderingApp.Model
+{
+    public class Restaurant
+    {
+        [PrimaryKey, AutoIncrement]
+        public int RestaurantID { get; set; }
+        public string RestaurantName { get; set; }
+    }
+}
