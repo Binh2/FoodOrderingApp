74722e15a30948d6d59f1148467a789300f97a67
Binh2 <hgqbinh2002@gmail.com>
2022-12-24 10:22:55 +0700

    Add UserState to User Add USER_STATE constant to hold available UserState

--- FoodOrderingApp/FoodOrderingApp/Model/User.cs
+++ FoodOrderingApp/FoodOrderingApp/Model/User.cs
@@ -10,12 +10,20 @@ namespace FoodOrderingApp.Model
         [PrimaryKey, AutoIncrement]
         public int UserID { get; set; }
         public string UserName { get; set; }
+        public string UserUsename { get; set; }
         public string UserEmail { get; set; }
         public string UserImage { get; set; }
         public string UserPassword { get; set; }
+        public int UserType { get; set; }
     }
     static public class UserProvider
     {
         static public User user;
+    }
+    static public class USER_TYPE
+    {
+        static public int CONSUMER = 0;
+        static public int PRODUCER = 1;
+        static public int ADMIN = 2;
     }
 }
