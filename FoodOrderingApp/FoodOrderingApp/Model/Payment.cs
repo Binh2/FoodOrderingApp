a6259984dde9b964fb440d492c87b32e1a76f583
Binh2 <hgqbinh2002@gmail.com>
2022-12-30 21:04:57 +0700

    Commit all the files that not conflict

---
+++ FoodOrderingApp/FoodOrderingApp/Model/Payment.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FoodOrderingApp.Model
+{
+    public class Payment
+    {
+        public int IdPayment { get; set; }
+    }
+}
